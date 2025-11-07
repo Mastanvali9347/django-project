672e73bae9ad3572f3bc27ea69d614101b5803e8
Mastan <patanmastan455@gmail.com>
2025-11-07 15:43:27 +0530

    myproject loc cnage

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rvomk@5yb+pq@8tu$7y+$+%qvoj4(0^8p=dmy%^@5sgga5rsvb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
